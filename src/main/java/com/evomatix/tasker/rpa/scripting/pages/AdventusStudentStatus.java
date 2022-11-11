e5ee0867d17596d4d44f9d35f943bfdc445fa05a
Vimukthi Hewapathirana <vimunet@gmail.com>
2022-11-11 15:48:25 +0530

    updated locator

--- src/main/java/com/evomatix/tasker/rpa/scripting/pages/AdventusStudentStatus.java
+++ src/main/java/com/evomatix/tasker/rpa/scripting/pages/AdventusStudentStatus.java
@@ -14,7 +14,7 @@ public class AdventusStudentStatus extends Page {
     public static ObjectLocator lnk_AppStages = new ObjectLocator("lnk_AppStages","//a[text()='#{{idf_AppStage}}#']", LocatorType.XPATH);
     public static ObjectLocator lnk_StudentName = new ObjectLocator("lnk_StudentName","//tr/td/span[contains(text(),'#{{idf_StudentID}}#')]/following::a[contains(text(),'#{{idf_StudentName}}#')]", LocatorType.XPATH);
     public static ObjectLocator txt_Search = new ObjectLocator("txt_Search","//span[contains(text(),'Search')]/following-sibling::input[@type='search']", LocatorType.XPATH);
-    public static ObjectLocator lnk_StudentNameByID = new ObjectLocator("lnk_StudentNameByID","//a[@href='/counsellor/student/#{{idf_StudentID}}#/show']", LocatorType.XPATH);
+    public static ObjectLocator lnk_StudentNameByID = new ObjectLocator("lnk_StudentNameByID","(//a[contains(@href,'/student/#{{idf_StudentID}}#/show')])[1]", LocatorType.XPATH);
     public static ObjectLocator lnk_Application = new ObjectLocator("lnk_Application","//a/span[text()='Applications']", LocatorType.XPATH);
     public static ObjectLocator lnk_SearchApply = new ObjectLocator("lnk_SearchApply","//div[text()='Search & Apply']", LocatorType.XPATH);
     public static ObjectLocator lnk_Documents= new ObjectLocator("lnk_Documents","//a/span[text()='Documents']", LocatorType.XPATH);
